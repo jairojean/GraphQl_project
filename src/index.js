b7de4ebda24f7e1d6d412590fddce76ffdb93b12
jairo jean <jairojean.br@gmail.com>
2025-01-17 08:41:03 -0300

    feat: Create Users

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
-const {ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql } = require('apollo-server');
 
-const typeDefs = gql``;
-const resolvers = {};
+const typeDefs = gql`
+type User {
+    id: ID
+    name: String
+    email: String
+    active: Boolean
+}
 
-const server = new ApolloServer({typeDefs, resolvers });
+type Post {
+    id: ID
+    title: String
+    content: String
+    author: [User!]!
+}
+
+type Query {
+    hello: String
+    users: [user]
+    getUserByEmail(email: String!): User
+}
+`;
+
+const users = [
+    {
+        _id: 'fghfgh',name: 'Jairo Jean',email: 'root@root.com', active: true
+    }
+];
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world',
+        users: () => users,
+        getUserByEmail: (_, args) => {
+            return users.find((user) => user.email === args.email);
+        }
+    }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
 server.listen().then(({ url }) => console.log(`Server started at ${url}`));
